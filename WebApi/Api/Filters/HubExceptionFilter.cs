1f8f172c56feb4cd4343570de8bf72dbe29e174c
Piotr <hynas321@gmail.com>
2025-04-30 22:42:15 +0200

    Add HubExceptionFilter, change docker-compose file

---
+++ WebApi/Api/Filters/HubExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApi.Api.Filters;
+
+public class HubExceptionFilter : IHubFilter
+{
+    private readonly ILogger<HubExceptionFilter> _logger;
+
+    public HubExceptionFilter(ILogger<HubExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<object> InvokeMethodAsync(
+        HubInvocationContext invocationContext,
+        Func<HubInvocationContext, ValueTask<object>> next)
+    {
+        try
+        {
+            return await next(invocationContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in Hub: '{Hub}', method: '{Method}'",
+                invocationContext.Hub.GetType().Name,
+                invocationContext.HubMethodName);
+
+            throw new HubException("An internal server error occurred");
+        }
+    }
+}
